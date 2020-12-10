c82b51733938166a5b25d1d2fb79800db76fc9ce
Benny W <12177862+BennyW23@users.noreply.github.co>
2020-12-09 21:29:47 -0800

    Reformatted q10 solution

--- 2020/q10.py
+++ 2020/q10.py
@@ -14,9 +14,9 @@ values = sorted(values)
 one_off = 0
 three_off = 0
 for i in range(len(values) - 1):
-    if values[i+1] - values[i] == 1:
+    if values[i + 1] - values[i] == 1:
         one_off += 1
-    elif values[i+1] - values[i] == 3:
+    elif values[i + 1] - values[i] == 3:
         three_off += 1
 
 print(one_off * three_off)
@@ -28,6 +28,8 @@ cache[values[0]] = 1
 for number in values:
     if number == values[0]:
         continue
-    cache[number] = cache.get(number + 1, 0) + cache.get(number + 2, 0) + cache.get(number + 3, 0)
+    cache[number] = (
+        cache.get(number + 1, 0) + cache.get(number + 2, 0) + cache.get(number + 3, 0)
+    )
 
 print(cache[0])
