1dcc2fd47b5a0bcd7e8cc5eb2136e8da4d6aebd0
Benny Wong <12177862+BennyW23@users.noreply.github.com>
2020-12-18 18:56:08 -0800

    Advent of code day 17 and 18

---
+++ 2020/q17.py
@@ -0,0 +1,79 @@
+import itertools
+
+
+def print_generation(gen, four_dimensions):
+    x_min = min([val[0] for val in gen])
+    x_max = max([val[0] for val in gen])
+    y_min = min([val[1] for val in gen])
+    y_max = max([val[1] for val in gen])
+    z_min = min([val[2] for val in gen])
+    z_max = max([val[2] for val in gen])
+    if four_dimensions:
+        w_min = min([val[2] for val in gen])
+        w_max = max([val[2] for val in gen])
+    else:
+        w_min = 0
+        w_max = 0
+
+    for w in range(w_min, w_max + 1):
+        for z in range(z_min, z_max + 1):
+            print("z=%d, w=%d" % (z, w))
+            for y in range(y_min, y_max + 1):
+                for x in range(x_min, x_max + 1):
+                    if (x, y, z) in gen:
+                        print("#", end="")
+                    else:
+                        print(".", end="")
+                print()
+            print()
+
+
+def get_next_gen(active, neighbor_directions, dimensions):
+    next_gen = set()
+    neighbors = {}
+
+    for active_cube in active:
+        for direction in neighbor_directions:
+            neighbor = tuple(
+                [active_cube[dim] + direction[dim] for dim in range(dimensions)]
+            )
+            neighbors[neighbor] = neighbors.get(neighbor, 0) + 1
+
+    for cube, count in neighbors.items():
+        if cube in active:
+            if count in range(2, 4):
+                next_gen.add(cube)
+        elif count == 3:
+            next_gen.add(cube)
+    return next_gen
+
+
+inp = open("q17_input.txt", "r").read().split()
+
+directions = set(itertools.product(range(-1, 2), repeat=3))
+directions.remove((0, 0, 0))
+
+active = set()
+for y in range(len(inp)):
+    for x in range(len(inp[y])):
+        if inp[y][x] == "#":
+            active.add((x, y, 0))
+
+for iteration in range(6):
+    active = get_next_gen(active, directions, 3)
+
+print(len(active))
+
+directions = set(itertools.product(range(-1, 2), repeat=4))
+directions.remove((0, 0, 0, 0))
+
+active = set()
+for y in range(len(inp)):
+    for x in range(len(inp[y])):
+        if inp[y][x] == "#":
+            active.add((x, y, 0, 0))
+
+for iteration in range(6):
+    active = get_next_gen(active, directions, 4)
+
+print(len(active))
---
+++ 2020/q18.py
@@ -0,0 +1,67 @@
+equations = open("q18_input.txt", "r").read().strip().split("\n")
+
+
+def eval_helper_1(equation):
+    # print("eval_helper", ''.join(equation))
+    while len(equation) > 1:
+        result = eval(equation[0] + equation[1] + equation[2])
+        equation.pop(0)
+        equation.pop(0)
+        equation.pop(0)
+        equation.insert(0, str(result))
+    return equation[0]
+
+
+def eval_helper_2(equation):
+    # print("eval_helper", ''.join(equation))
+    if len(equation) == 1:
+        return equation[0]
+    if "*" in equation:
+        index = equation.index("*")
+        return str(
+            eval(
+                eval_helper_2(equation[:index])
+                + "*"
+                + eval_helper_2(equation[index + 1 :])
+            )
+        )
+    return str(eval("".join(equation)))
+
+
+def evaluate(string, version):
+    # print('evaluate: ', string)
+    while "(" in string:
+        beginning_index, ending_index = find_matching_parentheses(string)
+        string = (
+            string[:beginning_index]
+            + evaluate(string[beginning_index + 1 : ending_index], version)
+            + string[ending_index + 1 :]
+        )
+    if version == 2:
+        result = eval_helper_2(string.split(" "))
+    else:
+        result = eval_helper_1(string.split(" "))
+    return result
+
+
+def find_matching_parentheses(string):
+    beginning_index = string.index("(")
+    index = beginning_index
+    count = 1
+    while count != 0:
+        index += 1
+        if string[index] == "(":
+            count += 1
+        elif string[index] == ")":
+            count -= 1
+    return beginning_index, index
+
+
+total_1 = 0
+total_2 = 0
+for equation in equations:
+    total_1 += int(evaluate(equation, 1))
+    total_2 += int(evaluate(equation, 2))
+
+print(total_1)
+print(total_2)
