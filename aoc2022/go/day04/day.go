452c4896a7d920fcc9c91f347f5e3cedd7d70af1
Benny Wong <12177862+BennyW23@users.noreply.github.com>
2023-10-23 01:03:58 -0400

    day 4 done

--- aoc2022/go/day04/day.go
+++ aoc2022/go/day04/day.go
@@ -1,19 +1,70 @@
-package dayXX
+package day04
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"goutil"
 )
 
 func Solve() {
-	fmt.Printf("\nDay XX: \n")
-	stringData := goutil.ReadFile("./aoc2022/input/dayXX.txt")
-	_ = stringData
+	fmt.Printf("\nDay 04: \n")
+	stringData := goutil.ReadFile("./aoc2022/input/day04.txt")
 
-	partOneAns := ""
+	containsPairs := 0
+	overlappingPairs := 0
+	for _, pair := range stringData {
+		data := strings.Split(pair, ",")
+		a := strings.Split(data[0], "-")
+		b := strings.Split(data[1], "-")
+
+		aStart, _ := strconv.Atoi(a[0])
+		aEnd, _ := strconv.Atoi(a[1])
+		bStart, _ := strconv.Atoi(b[0])
+		bEnd, _ := strconv.Atoi(b[1])
+
+		aContainsB, bContainsA := eachContains([]int{aStart, aEnd}, []int{bStart, bEnd})
+		if aContainsB || bContainsA {
+			containsPairs += 1
+		}
+
+		if hasOverlap([]int{aStart, aEnd}, []int{bStart, bEnd}) {
+			overlappingPairs += 1
+		}
+	}
+
+	partOneAns := containsPairs
 	fmt.Printf("Part 1: %v\n", partOneAns)
 
-	partTwoAns := ""
+	partTwoAns := overlappingPairs
 	fmt.Printf("Part 2: %v\n", partTwoAns)
+}
+
+func eachContains(a []int, b []int) (aContainsB bool, bContainsA bool) {
+	if a[0] <= b[0] && b[1] <= a[1] {
+		aContainsB = true
+	}
+	if b[0] <= a[0] && a[1] <= b[1] {
+		bContainsA = true
+	}
+
+	return aContainsB, bContainsA
+}
+
+func hasOverlap(a []int, b []int) bool {
+	if a[0] <= b[0] && b[0] <= a[1] {
+		return true
+	}
+	if a[0] <= b[1] && b[1] <= a[1] {
+		return true
+	}
+	if b[0] <= a[0] && a[0] <= b[1] {
+		return true
+	}
+	if b[0] <= a[1] && a[1] <= b[1] {
+		return true
+	}
+
+	return false
 }
--- aoc2022/go/main.go
+++ aoc2022/go/main.go
@@ -4,10 +4,12 @@ import (
 	"2022/day01"
 	"2022/day02"
 	"2022/day03"
+	"2022/day04"
 )
 
 func main() {
 	day01.Solve()
 	day02.Solve()
 	day03.Solve()
+	day04.Solve()
 }
